4224d42d0506bdccaa3b940f470cb5b9695b49bf
Tirthajyoti Ghosh <itirthahere@gmail.com>
2021-01-16 18:41:02 +0530

    Update styles in sidenav

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -8,13 +8,16 @@ const SideNav = () => (
   <div className="sidenav">
     <a href="/" className="logo"><img src="https://user-images.githubusercontent.com/57726348/95655344-3ae8de00-0b24-11eb-9c34-1fac11fecfcc.png" alt="logo" /></a>
 
-    <section className="types-list">
+    <section className="nav-items">
       <a href="#">Home</a>
       <NavLink exact to="/jobs" activeClassName="active">Search Job</NavLink>
       <NavLink exact to="/people" activeClassName="active">Find People</NavLink>
       <a href="#">Community</a>
       <a href="#">More</a>
     </section>
+
+
+    <a href="#" style={{ marginBottom: '50%' }}>Login</a>
   </div>
 );
 
