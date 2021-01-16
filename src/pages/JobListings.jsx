32f2ef1b0a0832007d2c0ab775715a08ba5f2d97
Tirthajyoti Ghosh <itirthahere@gmail.com>
2021-01-16 22:31:55 +0530

    Add jop listings page

--- src/pages/JobListings.jsx
+++ src/pages/JobListings.jsx
@@ -1,8 +1,153 @@
 import React from 'react';
 import Search from '../components/Search';
 
-const JobListings = () => {
-  return <Search />;
-};
+const JobListings = () => (
+  <>
+    <Search />
+
+    <section className="job-listings">
+      <div className="job-card">
+        <div className="company">
+          <div className="img">
+            <img src="https://torre-media.s3-us-west-2.amazonaws.com/Stripe.jfif" alt="company" />
+          </div>
+
+          <div className="info">
+            <h3>Senior UI/UX Designer</h3>
+            <p>Apple</p>
+          </div>
+        </div>
+        <div>
+          <h3>Cupertino, USA</h3>
+          <p>Location</p>
+        </div>
+        <div>
+          <h3>$25,000-30,000</h3>
+          <p>Salary</p>
+        </div>
+      </div>
+      <div className="job-card active">
+        <div className="company">
+          <div className="img">
+            <img src="https://res.cloudinary.com/torre-technologies-co/image/upload/v1600924860/origin/bio/organizations/quipu_logo_yikrag.png" alt="company" />
+          </div>
+
+          <div className="info">
+            <h3>Senior UI/UX Designer</h3>
+            <p>Apple</p>
+          </div>
+        </div>
+        <div>
+          <h3>Cupertino, USA</h3>
+          <p>Location</p>
+        </div>
+        <div>
+          <h3>$25,000-30,000</h3>
+          <p>Salary</p>
+        </div>
+      </div>
+      <div className="job-card">
+        <div className="company">
+          <div className="img">
+            <img src="https://torre-media.s3-us-west-2.amazonaws.com/Stripe.jfif" alt="company" />
+          </div>
+
+          <div className="info">
+            <h3>Senior UI/UX Designer</h3>
+            <p>Apple</p>
+          </div>
+        </div>
+        <div>
+          <h3>Cupertino, USA</h3>
+          <p>Location</p>
+        </div>
+        <div>
+          <h3>$25,000-30,000</h3>
+          <p>Salary</p>
+        </div>
+      </div>
+      <div className="job-card">
+        <div className="company">
+          <div className="img">
+            <img src="https://torre-media.s3-us-west-2.amazonaws.com/Stripe.jfif" alt="company" />
+          </div>
+
+          <div className="info">
+            <h3>Senior UI/UX Designer</h3>
+            <p>Apple</p>
+          </div>
+        </div>
+        <div>
+          <h3>Cupertino, USA</h3>
+          <p>Location</p>
+        </div>
+        <div>
+          <h3>$25,000-30,000</h3>
+          <p>Salary</p>
+        </div>
+      </div>
+      <div className="job-card">
+        <div className="company">
+          <div className="img">
+            <img src="https://torre-media.s3-us-west-2.amazonaws.com/Stripe.jfif" alt="company" />
+          </div>
+
+          <div className="info">
+            <h3>Senior UI/UX Designer</h3>
+            <p>Apple</p>
+          </div>
+        </div>
+        <div>
+          <h3>Cupertino, USA</h3>
+          <p>Location</p>
+        </div>
+        <div>
+          <h3>$25,000-30,000</h3>
+          <p>Salary</p>
+        </div>
+      </div>
+      <div className="job-card">
+        <div className="company">
+          <div className="img">
+            <img src="https://torre-media.s3-us-west-2.amazonaws.com/Stripe.jfif" alt="company" />
+          </div>
+
+          <div className="info">
+            <h3>Senior UI/UX Designer</h3>
+            <p>Apple</p>
+          </div>
+        </div>
+        <div>
+          <h3>Cupertino, USA</h3>
+          <p>Location</p>
+        </div>
+        <div>
+          <h3>$25,000-30,000</h3>
+          <p>Salary</p>
+        </div>
+      </div>
+      <div className="job-card">
+        <div className="company">
+          <div className="img">
+            <img src="https://torre-media.s3-us-west-2.amazonaws.com/Stripe.jfif" alt="company" />
+          </div>
+
+          <div className="info">
+            <h3>Senior UI/UX Designer</h3>
+            <p>Apple</p>
+          </div>
+        </div>
+        <div>
+          <h3>Cupertino, USA</h3>
+          <p>Location</p>
+        </div>
+        <div>
+          <h3>$25,000-30,000</h3>
+          <p>Salary</p>
+        </div>
+      </div>
+    </section>
+  </>
+);
 
 export default JobListings;
