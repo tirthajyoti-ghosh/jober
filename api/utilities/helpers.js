039217d5ff8f4dc8a51c85ba15e0ae1ea391a383
Tirthajyoti Ghosh <itirthahere@gmail.com>
2021-09-21 23:11:43 +0530

    Create helpers file; create separate file to store job site urls

---
+++ api/utilities/helpers.js
@@ -0,0 +1,11 @@
+const axios = require('axios');
+
+module.exports = {
+    createAxiosInstance: () => (
+        axios.create({
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36',
+            },
+        })
+    ),
+};
---
+++ api/utilities/urls.js
@@ -0,0 +1,9 @@
+module.exports = {
+    home: {
+        'remote-co': 'https://remote.co/remote-jobs/developer/',
+        'remoteok-io': 'https://remoteok.io/',
+        'remotive-io': `https://oqubrx6zeq-dsn.algolia.net/1/indexes/*/queries?x-algolia-agent=Algolia%20for%20JavaScript%20(4.0.0)%3B%20Browser%20(lite)&x-algolia-api-key=${process.env.REMOTIVE_IO_ALGOLIA_API_KEY}&x-algolia-application-id=${process.env.REMOTIVE_IO_ALGOLIA_APPLICATION_ID}`,
+        'weworkremotely-com': 'https://weworkremotely.com/',
+        'workingnomads-co': 'https://www.workingnomads.co/jobsapi/job/_search?sort=expired:asc,premium:desc,pub_date:desc&_source=company,category_name,description,location_base,salary_range,salary_range_short,instructions,id,external_id,slug,title,pub_date,tags,source,apply_url,premium,expired,use_ats,position_type&size=100&from=0&q=(category_name.raw:%22Development%22)',
+    },
+};
