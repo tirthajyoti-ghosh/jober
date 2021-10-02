9688d843e5ea2c9fb8431ba18fcfdc10822a0aaf
Tirthajyoti Ghosh <itirthahere@gmail.com>
2021-10-02 15:39:07 +0530

    Add remote.co search results scraper

---
+++ api/scrapers/remote-co/search.js
@@ -0,0 +1,63 @@
+/* eslint-disable max-len */
+const cheerio = require('cheerio');
+const querystring = require('querystring');
+
+const urls = require('../../utilities/urls');
+const selectors = require('../../utilities/html-selectors');
+const { createAxiosInstance } = require('../../utilities/helpers');
+
+module.exports = async (keywords) => {
+    try {
+        const axios = createAxiosInstance();
+
+        const response = await axios.post(urls.search['remote-co'],
+            querystring.stringify({
+                search_keywords: keywords,
+                per_page: 50,
+            }), {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+            });
+
+        // If the "found_jobs" property is false, then return an empty array
+        if (!response.data.found_jobs) {
+            return [];
+        }
+
+        const $ = cheerio.load(response.data.html);
+
+        const {
+            job, title, type, region, url, company, logo, logoAttr,
+        } = selectors['remote-co'].search;
+
+        const data = [];
+        $(job).each(function jobSection() { // The "this" keyword won't work if arrow function is used
+            const jobTitle = $(title, this).text().trim();
+            const jobType = $(type, this).text().trim();
+            const jobRegion = $(region, this).text().trim();
+            const jobURL = $(url, this).attr('href');
+
+            const companyTitle = $(company, this).text().trim();
+            const companyLogo = $(logo, this).attr(logoAttr) || '';
+
+            data.push({
+                job: {
+                    title: jobTitle,
+                    type: jobType,
+                    region: jobRegion,
+                    url: jobURL,
+                },
+                company: {
+                    title: companyTitle,
+                    logo: companyLogo,
+                },
+            });
+        });
+
+        return data;
+    } catch (error) {
+        console.log(error);
+        return error;
+    }
+};
